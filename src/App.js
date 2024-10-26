ff24413a035416e7bb47b91d205488240d7c3992
amitNits <jscoder2023@gmail.com>
2024-10-26 15:29:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+// import { ToastContainer, toast } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen m-0 p-0 overflow-x-hidden">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      
+      <Toaster
+        position="top-center"
+        reverseOrder={true}/>
     </div>
   );
 }
